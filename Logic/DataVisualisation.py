b9e57192c4cdc8caa28008e61558a2b12431a810
jackow98 <jw50@hw.ac.uk>
2019-11-29 10:22:56 +0000

    Played about with labels of charts

--- Logic/DataVisualisation.py
+++ Logic/DataVisualisation.py
@@ -6,13 +6,25 @@ import matplotlib.pyplot as plt
 class DataVisualisation:
 
     @staticmethod
-    def create_histogram(item_to_count_dict: dict, title: str):
+    def create_histogram(item_to_count_dict: dict, title: str, x_label="", y_label=""):
         """
         Using matplot lib, generate and display a histogram
         :param item_to_count_dict:
+        :param title:
+        :param x_label:
+        :param y_label:
         :return:
         """
         plt.bar(range(len(item_to_count_dict)), list(item_to_count_dict.values()), align='center')
-        plt.xticks(range(len(item_to_count_dict)), list(item_to_count_dict.keys()))
+        # TODO: Make display of labels pretty
+        plt.xticks(
+            range(len(item_to_count_dict)),
+            list(item_to_count_dict.keys()),
+            horizontalalignment='center',
+            verticalalignment='center',
+            wrap=True
+        )
+        plt.xlabel(x_label, fontsize=14)
+        plt.ylabel(y_label, fontsize=14)
         plt.title(title)
         plt.show()
